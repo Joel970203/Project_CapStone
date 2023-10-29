00b2a784c1e49fb2ae218143b7854ef494f6fe2c
Rubbe1124 <nbj10@naver.com>
2023-10-29 23:03:05 +0900

    2023/10/29 - 나영빈 - 카메라 구도 변경
    ---Camera_Move 스크립트 추가---
    카메라 위치는 고정
    타겟과 자신 캐릭터 사이 방향 벡터를 구함
    구한 방향 벡터에 y값은 0으로 초기화하고, 캐릭터의 정면을 방향 벡터 구한 쪽으로 이동하게 업데이트 문을 코딩
    
    이대로하면 캐릭터 이동 시의 모습이 어색해짐. 카메라 구도는 동일하게 진행하고 애니메이션과 캐릭터 정면 바라보는 각도 수정 필요
    
    # Conflicts:
    #	Assets/Scenes/Game.unity

---
+++ Assets/Character/Script/Camera_Move.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotationOffset = target.transform.position - this.gameObject.transform.position;
+        rotationOffset.y=0;
+        this.gameObject.transform.forward+=Vector3.Lerp(this.transform.forward,rotationOffset,Time.deltaTime*20f);
+    }
+}
