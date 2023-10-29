354f1f26e722ac569b1ed2f0406d458856f4ffa5
Rubbe1124 <nbj10@naver.com>
2023-10-29 15:07:26 +0900

    2023/10/29 - 나영빈 - 캐릭터 설계
    ---Character_Info 스크립트 수정---
    피격시 이펙트 생성 함수 추가.
    보스 태그의 콜리더 접촉 시 자신과 보스의 중간 위치에 타격 이펙트 생성하도록 함.
    
    ---캐릭터 애니메이션 변경---
    저번 회의때 결정된 내용으로 걷는 애니메이션이 캐릭터의 속도에 비해 너무 느린것같아서 뛰는 모션으로 변경함.

--- Assets/Character/Script/Character_Info.cs
+++ Assets/Character/Script/Character_Info.cs
@@ -13,6 +13,9 @@ public class Character_Info : MonoBehaviour
     [SerializeField]
     private int armor;
 
+    [SerializeField]
+    private GameObject hitEffectPrefab;
+
     void Start()
     {
 
@@ -23,6 +26,25 @@ public class Character_Info : MonoBehaviour
     {
 
     }
+
+    //보스에게 근접 공격 피격시 작동하는 이펙트
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Boss"))
+        {
+            Vector3 characterCenter = transform.position; // 캐릭터 중심점
+            Vector3 BossCenter = other.gameObject.transform.position; // 적의 중심점
+
+            // 타격 위치를 평균 위치로 설정
+            Vector3 hitPoint = (characterCenter + BossCenter) / 2;
+
+            // 타격 이펙트를 생성하거나 처리
+            GameObject spawnedHit =Instantiate(hitEffectPrefab, hitPoint, Quaternion.identity);
+
+            TakeDamage(20);
+        }
+    }
+
 
     public virtual void TakeDamage(int damage)
     {
