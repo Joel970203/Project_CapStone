83e412dd6f9c56f6ae5a7f0bb9c92f8fc5306f8e
1CM98 <148876482+1CM98@users.noreply.github.com>
2023-11-01 00:19:19 +0900

    11/1 인벤토리 버그 수정
    아이템 사용에 있어 버그를 수정

--- Assets/item/Scripts/Inventory.cs
+++ Assets/item/Scripts/Inventory.cs
@@ -12,87 +12,101 @@ using Image = UnityEngine.UI.Image;
 
 public class Inventory
 {
-    List<Item> items = new List<Item>();
+    Dictionary<ItemType, Item> itemList = new Dictionary<ItemType, Item>();
+    ItemType[] slotArr = new ItemType[6]; //슬롯 번호에 해당되는 아이템 타입의 정보가 들어간다.
+
     GameObject[] slotUI;
     bool dupFlag = false; //중복됐는지를 확인하는 플레그
+    bool notDupFlag = false; //중복되지 않는 아이템의 경우 슬롯의 index가 낮을 수록 우선순위를 부여하기 위한 flag
 
-    public void setSlotUI(GameObject[] slotUI)
+    public void setSlot(GameObject[] slotUI)
     {
-       this.slotUI = slotUI;
+        this.slotUI = slotUI;
+        for (int i = 0; i < 6; i++)
+        {
+            slotArr[i] = ItemType.Nothing;
+        }
     }
 
     public void AddItem(Item newItem)
     {
-        int index = 0; //i랑 따로 둔 이유는 나중에 자리 위치 바꿀 것 고려
-        int itemsCount = items.Count;
-        for (int i = 0; i < itemsCount; i++)
+        int notDupIndex = 0; //인벤토리 내에 추가되는 아이템과 중복되는 아이템이 없을 경우 사용하는 index
+        int itemListCount = itemList.Count;
+
+        ItemType newItemType = newItem.type;
+
+        for (int i = 0; i < 6; i++)
         {
-            Item existItem = items[i];
-
-            if (existItem == null)
+            if (i < itemListCount)
             {
-                itemsCount++;
-                continue;
-            }
+                if (slotArr[i] == ItemType.Nothing)
+                {
+                    if (!notDupFlag)
+                    {  //for문은 돌수록 index 번호가 커지기에 처음 빈 슬롯을 찾으면 만일 중복되지 않을 시에 슬롯을 해당 index 번호로 정해두기 위해 flag 설정 (우선순위는 index 번호가 낮은 것)
+                        notDupIndex = i;
+                        notDupFlag = true;
+                    }
 
-            if (existItem.type == newItem.type)
+                    itemListCount++;
+                    continue;
+                }
+
+                ItemType existItemType = slotArr[i];
+
+                if (existItemType == newItemType)
+                {
+                    itemList[existItemType].quantity++;
+
+                    TextMeshProUGUI quantityText = slotUI[i].transform.Find("QuantityText").GetComponentInChildren<TextMeshProUGUI>();
+                    quantityText.text = itemList[existItemType].quantity.ToString();
+
+                    dupFlag = true;
+
+                    break;
+                }
+            }
+            else
             {
-                existItem.quantity++;
-
-                TextMeshProUGUI quantityText = slotUI[i].transform.Find("QuantityText").GetComponentInChildren<TextMeshProUGUI>();
-                quantityText.text = existItem.quantity.ToString();
-
-                dupFlag = true;
-                break;
+                if (!notDupFlag)
+                {   
+                    notDupIndex = i;
+                    break;
+                }
             }
-
-            index++; //슬롯에 아이템이 없거나 중복된 아이템이 아닐때만 index를 더한다.
         }
 
         if (!dupFlag)
         {
-            slotUI[index].GetComponent<Image>().sprite = newItem.image;
-            items.Add(newItem);
+            slotUI[notDupIndex].GetComponent<Image>().sprite = newItem.image;
+            slotArr[notDupIndex] = newItemType;
+            itemList.Add(newItemType, newItem);
+            itemListCount++;
 
-            TextMeshProUGUI quantityText = slotUI[index].transform.Find("QuantityText").GetComponentInChildren<TextMeshProUGUI>();
+            TextMeshProUGUI quantityText = slotUI[notDupIndex].transform.Find("QuantityText").GetComponentInChildren<TextMeshProUGUI>();
             quantityText.text = "1";
         }
 
+        notDupFlag = false;
         dupFlag = false;
     }
 
     public ItemType UseItem(int index)
     {
-        try
+        if (slotArr[index] == ItemType.Nothing) return ItemType.Nothing;
+
+        ItemType useItemType = slotArr[index];
+        itemList[useItemType].quantity--;
+
+        TextMeshProUGUI quantityText = slotUI[index].transform.Find("QuantityText").GetComponentInChildren<TextMeshProUGUI>();
+        quantityText.text = itemList[useItemType].quantity.ToString();
+
+        if (itemList[useItemType].quantity == 0)
         {
-            if (items[index] == null) return ItemType.Nothing;
-
-
-            items[index].quantity--;
-            TextMeshProUGUI quantityText = slotUI[index].transform.Find("QuantityText").GetComponentInChildren<TextMeshProUGUI>();
-            quantityText.text = items[index].quantity.ToString();
-
-            if (items[index].quantity == 0)
-            {
-                slotUI[index].GetComponent<Image>().sprite = null;
-                items.RemoveAt(index);
-            }
-
-            return items[index].type;
+            itemList.Remove(useItemType);
+            slotUI[index].GetComponent<Image>().sprite = null;
+            slotArr[index] = ItemType.Nothing;
         }
-        catch
-        {
-            Debug.LogError("out of range");
-            return ItemType.Nothing;
-        }
-    }
 
-    public void print()
-    {
-        foreach (Item existItem in items)
-        {
-            Debug.Log("Item Type: " + existItem.type);
-            Debug.Log("Item quantity: " + existItem.quantity);
-        }
+        return useItemType;
     }
 }
